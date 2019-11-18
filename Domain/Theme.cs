f9445b716f245db6677dc0d3b0a44b150be6d086
TheAngryDuck <martin.oblikas@gmail.com>
2019-11-18 10:32:45 +0200

    add EF

---
+++ Domain/Theme.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Theme
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<TrackInTheme> TrackInThemes { get; set; } = new List<TrackInTheme>();
+    }
+}
---
+++ Domain/TrackInTheme.cs
@@ -0,0 +1,11 @@
+namespace Domain
+{
+    public class TrackInTheme
+    {
+        public Track Track { get; set; }
+        public int TrackId { get; set; }
+
+        public Theme Theme { get; set; }
+        public int ThemeId { get; set; }
+    }
+}
